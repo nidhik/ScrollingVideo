6fec116ed5c3dccea58f95e034841130cc9c60c7
Nidhi Kulkarni <nkulkarni@mux.com>
2021-07-12 22:34:58 -0400

    init

---
+++ ScrollingVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollingVideo
+//
+//  Created by Nidhi Kulkarni on 7/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
